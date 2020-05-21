c57c6176e8daefe9d9e5f9f42dd30525a328458a
aj mac <macdonalda47@gmail.com>
2020-05-21 16:07:14 -0500

    switch case inserted; movies search functional; begin bands functionality; keys protection

--- liri.js
+++ liri.js
@@ -1,6 +1,6 @@
 //At the top of the liri.js file, add code to read and set any environment variables with the dotenv package:
 
-// require("dotenv").config();
+require("dotenv").config();
 
 // Add the code required to import the keys.js file and store it in a variable.
 // var keys = require("./keys.js");
@@ -14,21 +14,108 @@ var inquirer = require("inquirer");
 // spotify-this-song
 // do-what-it-says
 //movie-this
+function searchMovie() {
+    inquirer
+      .prompt([
+        // Here we give the user a list to choose from.
+        {
+          type: "input",
+          message: "What movie would you like to search?",
+          name: "movie",
+        },
+      ])
+      .then(function (movieResponse) {
+          var searchTerm = movieResponse.movie
+          console.log(searchTerm);
+        // We then run the request with axios module on a URL with a JSON
+        axios
+          .get(
+            `http://www.omdbapi.com/?t=${searchTerm}&y=&plot=short&apikey=trilogy`)
+          
+          .then(function (response) {
+            // Then we print out the movie data
+            console.log(response.data);
+            console.log("The movie's title is: " + response.data.Title);
+            console.log("The year the movie came out is: " + response.data.Year);
+            console.log(
+              "The movie's IMDB rating is: " + response.data.imdbRating
+            );
+            console.log(
+              "The movie's Rotten Tomatoes rating is: " +
+                JSON.stringify(response.data.Ratings[1], ["Value"])
+            );
+            console.log("The movie was produced in: " + response.data.Country);
+            console.log("The movie language is: " + response.data.Language);
+            console.log("The movie's plot is: " + response.data.Plot);
+            console.log("The movie's actors are: " + response.data.Actors);
+          });
+      });
+  }
 
+  function searchBands() {
+    inquirer
+      .prompt([
+        // Here we give the user a list to choose from.
+        {
+          type: "input",
+          message: "What band would you like to search?",
+          name: "band",
+        },
+      ])
+      .then(function (bandResponse) {
+          var searchTerm = bandResponse.band
+          console.log(searchTerm);
+        // We then run the request with axios module on a URL with a JSON
+        axios
+          .get(
+            `https://rest.bandsintown.com/artists/" + searchTerm + "/events?app_id=${process.env.BINKEY}`)
+          
+          .then(function (response) {
+            // Then we print out the band data
+            console.log(response);
+
+          });
+      });
+  }
 //Create a "Prompt" with a series of questions.
-inquirer
-  .prompt([
-    // Here we give the user a list to choose from.
-    {
-      type: "list",
-      message: "What would you like to do?",
-      choices: ["movie-this", "concert-this", "spotify-this-song", "do-what-it-says"],
-      name: "options"
-    }
-  ])
+function getStarted() {
+  inquirer
+    .prompt([
+      // Here we give the user a list to choose from.
+      {
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "movie-this",
+          "concert-this",
+          "spotify-this-song",
+          "do-what-it-says",
+        ],
+        name: "options",
+      },
+    ])
+    .then(function (inquirerResponse) {
+      switch (inquirerResponse.options) {
+        case "movie-this":
+          searchMovie();
+          break;
+        case "concert-this":
+          searchBands();
+          break;
+        // case "spotify-this-song":
+        //   searchSpotify(searchInput);
+        //   break;
+        //   case "do-what-it-says":
+        //   searchDo(searchInput)
+        //   break;
+        default:
+        // code block
+      }
+    });
+}
 
- // .then(function(inquirerResponse) {
-    // console.log(inquirerResponse)
+
+getStarted();
 //     if (inquirerResponse.confirm) {
 //       console.log("\nWelcome " + inquirerResponse.username);
 //       console.log("Your " + inquirerResponse.pokemon + " is ready for battle!\n");
@@ -37,21 +124,3 @@ inquirer
 //       console.log("\nThat's okay " + inquirerResponse.username + ", come again when you are more sure.\n");
 //     }
 //   });
-
-
-// We then run the request with axios module on a URL with a JSON
-axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
-  function(response) {
-    // Then we print out the movie data
-    console.log(response.data);
-    console.log("The movie's title is: " + response.data.Title);
-    console.log("The year the movie came out is: " + response.data.Year);
-    console.log("The movie's IMDB rating is: " + response.data.imdbRating);
-    console.log("The movie's Rotten Tomatoes rating is: " + JSON.stringify(response.data.Ratings[1], ['Value']));
-    console.log("The movie was produced in: " + response.data.Country);
-    console.log("The movie language is: " + response.data.Language);
-    console.log("The movie's plot is: " + response.data.Plot);
-    console.log("The movie's actors are: " + response.data.Actors);
-  }
-);
-
