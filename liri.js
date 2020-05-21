36d3378386264e2dda1c46c6370a2d3e5300333b
aj mac <macdonalda47@gmail.com>
2020-05-21 15:00:59 -0500

    json split to grab rotten tomatoes rating; properly install inquirer; console.log movie data properly

--- liri.js
+++ liri.js
@@ -2,53 +2,33 @@
 
 // require("dotenv").config();
 
-// // Add the code required to import the keys.js file and store it in a variable.
+// Add the code required to import the keys.js file and store it in a variable.
 // var keys = require("./keys.js");
 
-// //You should then be able to access your keys information like so
-// // var spotify = new Spotify(keys.spotify);
-
-// //Make it so liri.js can take in one of the following commands:
-// // concert-this
-// // spotify-this-song
-// // do-what-it-says
-// movie-this
-
+//You should then be able to access your keys information like so
+//var spotify = new Spotify(keys.spotify);
 var axios = require("axios");
+var inquirer = require("inquirer");
+//Make it so liri.js can take in one of the following commands:
+// concert-this
+// spotify-this-song
+// do-what-it-says
+//movie-this
 
-// We then run the request with axios module on a URL with a JSON
-axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
-  function(response) {
-    // Then we print out the imdbRating
-    console.log(response.data);
-    console.log("The movie's title is: " + response.data.imdbRating);
-    console.log("The year the movie came out is: " + response.data.imdbRating);
-    console.log("The movie's IMDB rating is: " + response.data.imdbRating);
-    console.log("The movie's Rotten Tomatoes rating is: " + response.data.imdbRating);
-    console.log("The movie was produced in: " + response.data.imdbRating);
-    console.log("The movie language is: " + response.data.imdbRating);
-    console.log("The movie's plot is: " + response.data.imdbRating);
-    console.log("The movie's actors are: " + response.data.imdbRating);
-  }
-);
+//Create a "Prompt" with a series of questions.
+inquirer
+  .prompt([
+    // Here we give the user a list to choose from.
+    {
+      type: "list",
+      message: "What would you like to do?",
+      choices: ["movie-this", "concert-this", "spotify-this-song", "do-what-it-says"],
+      name: "options"
+    }
+  ])
 
-// var inquirer = require("inquirer");
-
-// // Create a "Prompt" with a series of questions.
-// inquirer
-//   .prompt([
-//     // Here we give the user a list to choose from.
-//     {
-//       type: "list",
-//       message: "Which Pokemon do you choose?",
-//       choices: ["movie-this", "concert-this", "spotify-this-song", "do-what-it-says"],
-//       name: "options"
-//     }
-//   ])
-
-//   .then(function(inquirerResponse) {
-//     // If the inquirerResponse confirms, we displays the inquirerResponse's username and pokemon from the answers.
-//     console.log(inquirerResponse)
+ // .then(function(inquirerResponse) {
+    // console.log(inquirerResponse)
 //     if (inquirerResponse.confirm) {
 //       console.log("\nWelcome " + inquirerResponse.username);
 //       console.log("Your " + inquirerResponse.pokemon + " is ready for battle!\n");
@@ -58,4 +38,20 @@ axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=tr
 //     }
 //   });
 
+
+// We then run the request with axios module on a URL with a JSON
+axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
+  function(response) {
+    // Then we print out the movie data
+    console.log(response.data);
+    console.log("The movie's title is: " + response.data.Title);
+    console.log("The year the movie came out is: " + response.data.Year);
+    console.log("The movie's IMDB rating is: " + response.data.imdbRating);
+    console.log("The movie's Rotten Tomatoes rating is: " + JSON.stringify(response.data.Ratings[1], ['Value']));
+    console.log("The movie was produced in: " + response.data.Country);
+    console.log("The movie language is: " + response.data.Language);
+    console.log("The movie's plot is: " + response.data.Plot);
+    console.log("The movie's actors are: " + response.data.Actors);
+  }
+);
 
