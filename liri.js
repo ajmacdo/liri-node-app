909f1fe7f8f06c89aa7a365778229b22eb68a016
aj mac <macdonalda47@gmail.com>
2020-05-21 13:48:53 -0500

    initiate axios functionality; begin imdb inquirer response code/functionality

--- liri.js
+++ liri.js
@@ -1,17 +1,17 @@
 //At the top of the liri.js file, add code to read and set any environment variables with the dotenv package:
 
-require("dotenv").config();
+// require("dotenv").config();
 
-// Add the code required to import the keys.js file and store it in a variable.
-var keys = require("./keys.js");
+// // Add the code required to import the keys.js file and store it in a variable.
+// var keys = require("./keys.js");
 
-//You should then be able to access your keys information like so
-var spotify = new Spotify(keys.spotify);
+// //You should then be able to access your keys information like so
+// // var spotify = new Spotify(keys.spotify);
 
-//Make it so liri.js can take in one of the following commands:
-// concert-this
-// spotify-this-song
-// do-what-it-says
+// //Make it so liri.js can take in one of the following commands:
+// // concert-this
+// // spotify-this-song
+// // do-what-it-says
 // movie-this
 
 var axios = require("axios");
@@ -20,34 +20,42 @@ var axios = require("axios");
 axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
   function(response) {
     // Then we print out the imdbRating
-    console.log("The movie's rating is: " + response.data.imdbRating);
+    console.log(response.data);
+    console.log("The movie's title is: " + response.data.imdbRating);
+    console.log("The year the movie came out is: " + response.data.imdbRating);
+    console.log("The movie's IMDB rating is: " + response.data.imdbRating);
+    console.log("The movie's Rotten Tomatoes rating is: " + response.data.imdbRating);
+    console.log("The movie was produced in: " + response.data.imdbRating);
+    console.log("The movie language is: " + response.data.imdbRating);
+    console.log("The movie's plot is: " + response.data.imdbRating);
+    console.log("The movie's actors are: " + response.data.imdbRating);
   }
 );
 
-var inquirer = require("inquirer");
+// var inquirer = require("inquirer");
 
-// Create a "Prompt" with a series of questions.
-inquirer
-  .prompt([
-    // Here we give the user a list to choose from.
-    {
-      type: "list",
-      message: "Which Pokemon do you choose?",
-      choices: ["movie-this", "concert-this", "spotify-this-song", "do-what-it-says"],
-      name: "options"
-    }
-  ])
-  
-  .then(function(inquirerResponse) {
-    // If the inquirerResponse confirms, we displays the inquirerResponse's username and pokemon from the answers.
-    console.log(inquirerResponse)
-    if (inquirerResponse.confirm) {
-      console.log("\nWelcome " + inquirerResponse.username);
-      console.log("Your " + inquirerResponse.pokemon + " is ready for battle!\n");
-    }
-    else {
-      console.log("\nThat's okay " + inquirerResponse.username + ", come again when you are more sure.\n");
-    }
-  });
+// // Create a "Prompt" with a series of questions.
+// inquirer
+//   .prompt([
+//     // Here we give the user a list to choose from.
+//     {
+//       type: "list",
+//       message: "Which Pokemon do you choose?",
+//       choices: ["movie-this", "concert-this", "spotify-this-song", "do-what-it-says"],
+//       name: "options"
+//     }
+//   ])
+
+//   .then(function(inquirerResponse) {
+//     // If the inquirerResponse confirms, we displays the inquirerResponse's username and pokemon from the answers.
+//     console.log(inquirerResponse)
+//     if (inquirerResponse.confirm) {
+//       console.log("\nWelcome " + inquirerResponse.username);
+//       console.log("Your " + inquirerResponse.pokemon + " is ready for battle!\n");
+//     }
+//     else {
+//       console.log("\nThat's okay " + inquirerResponse.username + ", come again when you are more sure.\n");
+//     }
+//   });
 
 
