5eb236e0d18f107f35a1d5967ad5a1f39a25ea45
aj mac <macdonalda47@gmail.com>
2020-08-06 14:19:32 -0500

    format code

--- liri.js
+++ liri.js
@@ -1,14 +1,11 @@
 //At the top of the liri.js file, add code to read and set any environment variables with the dotenv package:
-
 require("dotenv").config();
 
 // Add the code required to import the keys.js file and store it in a variable.
 var keys = require("./key");
 
-
 //You should then be able to access your keys information like so
 var Spotify = require("node-spotify-api");
-
 var spotify = new Spotify(keys.spotify);
 var axios = require("axios");
 var inquirer = require("inquirer");
@@ -30,10 +27,9 @@ function searchMovie() {
         axios
           .get(
             `http://www.omdbapi.com/?t=${searchTerm}&y=&plot=short&apikey=trilogy`)
-          
           .then(function (response) {
             // Then we print out the movie data
-            console.log(response.data);
+            // console.log(response.data);
             console.log("The movie's title is: " + response.data.Title);
             console.log("The year the movie came out is: " + response.data.Year);
             console.log("The movie's IMDB rating is: " + response.data.imdbRating);
